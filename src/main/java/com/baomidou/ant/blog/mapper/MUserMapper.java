75a572decc94e9407e25fc3b42a27860af3589d8
Carolyn <2535806990@qq.com>
2023-03-01 11:44:23 +0800

    自动代码生成器

---
+++ src/main/java/com/baomidou/ant/blog/mapper/MUserMapper.java
@@ -0,0 +1,16 @@
+package com.baomidou.ant.blog.mapper;
+
+import com.baomidou.ant.blog.entity.MUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户信息表 Mapper 接口
+ * </p>
+ *
+ * @author Carolyn
+ * @since 2023-03-01
+ */
+public interface MUserMapper extends BaseMapper<MUser> {
+
+}
--- src/main/java/com/config/MybatisPlusConfig.java
+++ src/main/java/com/config/MybatisPlusConfig.java
@@ -1,4 +1,4 @@
-package com.carolyn.blog.config;
+package com.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
